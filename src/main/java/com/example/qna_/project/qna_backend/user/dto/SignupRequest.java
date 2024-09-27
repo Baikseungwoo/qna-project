f75d1a3665f1ff82309bda544665e06395732638
seungwoo <swbaik01@naver.com>
2024-09-27 10:37:31 +0900

    week1 update

--- src/main/java/com/example/qna_/project/qna_backend/user/dto/SignupRequest.java
+++ src/main/java/com/example/qna_/project/qna_backend/user/dto/SignupRequest.java
@@ -1,5 +1,6 @@
 package com.example.qna_.project.qna_backend.user.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
@@ -8,11 +9,18 @@ import lombok.Getter;
 @Getter
 @Builder
 public class SignupRequest {
+
     @Size(min = 3, max = 25)
-    @NotEmpty(message = "사용자 ID는 필수입니다.")
+    @NotEmpty(message = "사용자ID는 필수항목입니다.")
     private String username;
 
-    @NotEmpty(message = "비밀번호는 필수입니다.")
+    @NotEmpty(message = "비밀번호는 필수항목입니다.")
+    private String password1;
 
+    @NotEmpty(message = "비밀번호 확인은 필수항목입니다.")
+    private String password2;
 
+    @NotEmpty(message = "이메일은 필수항목입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    private String email;
 }
